c685292b043dd789747c28511e5cb819ac4f6ffd
nkumarcalm <narinder.calm@outlook.com>
2026-05-27 23:39:19 +0530

    fixed clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpg|jpeg|gif|png|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!.*\\..*|_next).*)',
+    '/',
     '/(api|trpc)(.*)',
   ],
 }
